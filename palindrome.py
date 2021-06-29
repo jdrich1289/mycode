ddbc57a1e90a2019072432d605f3a98b41950a32
John Richardson <jdrichardson1289@gmail.com>
2021-06-29 10:02:29 -0700

    testing lists

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+# working with lists
+my_list = ["192.168.0.5", 5060, "UP"]
+# print first item
+print("The first item in the list (IP): " + my_list[0])
+# print second item
+print("The second item in the list (port): " + str(my_list[1]))
+# print last item
+print("The last item in the list (state): " + my_list[2])
+
+# challenge
+print()
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("The ip addrs are: " + iplist[3] + " and " + iplist[4])
