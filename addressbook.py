85a3a1c1584bfccd973d3f829ad78f363bd8fd4c
John Richardson <jdrichardson1289@gmail.com>
2021-06-29 15:13:20 -0700

    palindrome

---
+++ addressbook.py
@@ -0,0 +1,3 @@
+claude = [{"name": "Claude Shannon", "street": "5 Cambridge St", "city": "Winchester", "state": "MA", "zip": "01890", "phone": "555-1212", "email": "bitplayer@mit.com"}]
+
+print(claude[0]["email"])
--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,29 @@
-# working with lists
-my_list = ["192.168.0.5", 5060, "UP"]
-# print first item
-print("The first item in the list (IP): " + my_list[0])
-# print second item
-print("The second item in the list (port): " + str(my_list[1]))
-# print last item
-print("The last item in the list (state): " + my_list[2])
+origin = []
+bob = input("Enter a palindrome: ")
 
-# challenge
-print()
-iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
-print("The ip addrs are: " + iplist[3] + " and " + iplist[4])
+for i in bob:
+    if i == ' ':
+        print()
+    else:
+        origin.append(i)
+
+boblower = bob.lower()
+boblist = []
+boblist.extend(boblower)
+boblist.reverse()
+bobrev = []
+
+for i in boblist:
+    if i == ' ':
+        print()
+    else:
+        bobrev.append(i)
+
+seperator = ""
+bobrevcomb = (seperator.join(bobrev))
+boblistcomb = (seperator.join(origin))
+
+if boblistcomb == bobrevcomb:
+    print(bob, "is a palindrome.")
+else:
+    print(bob, "is not a palindrom.")
