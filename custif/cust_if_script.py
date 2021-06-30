5f2591dcf4689007771c6901b7f19809c561dfd3
John Richardson <jdrichardson1289@gmail.com>
2021-06-30 16:52:24 +0000

    lab work

---
+++ custif/cust_if_script.py
@@ -0,0 +1,12 @@
+score = int(input("What is your grade?: "))
+
+if score >= 90: 
+    print("You have an A.")
+elif score >= 80:
+    print("You have a B.")
+elif score >= 70: 
+    print("you have a C.")
+elif score >= 60:
+    print("You have a D.")
+else:
+    print("You have a F.")
--- moveplease01.py
+++ moveplease01.py
@@ -10,7 +10,7 @@ import os       # provies access to low level os operations (agnostic to flavor 
 def main():
      """called at runtime"""
      os.chdir('/home/student/mycode/')   # move into this working directory
-     shutil.move('raynor.obj', 'ceph_storage/')  # try moving the file raynor.obj into ceph_storage/ dir
+     shutil.move('raynor.obj', 'ceph_storage/raynor.obj')  # try moving the file raynor.obj into ceph_storage/ dir
 
      # program will pause while we accept input
      xname = input('What is the new name for kerrigan.obj? ') # collect string input from the user
